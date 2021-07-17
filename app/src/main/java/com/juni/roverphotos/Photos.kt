a83c5cb64ed5fec96f8bec15b2dc590612506bec
JuninhoA <juninhowyx@gmail.com>
2021-07-17 15:33:04 -0500

    *agregada url de las fotos

--- app/src/main/java/com/juni/roverphotos/Photos.kt
+++ app/src/main/java/com/juni/roverphotos/Photos.kt
@@ -1,6 +1,6 @@
 package com.juni.roverphotos
 
 
-data class Photos(val id:String,val earth_date: String,val camera_name:String) {
+data class Photos(val id:String,val earth_date: String,val camera_name:String,val img_src:String) {
 
 }
