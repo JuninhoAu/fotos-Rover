51163de6a280f9583cf60798edccc85b15c8677a
JuninhoA <juninhowyx@gmail.com>
2021-07-17 16:06:23 -0500

    *agregado onItemSelected

--- app/src/main/java/com/juni/roverphotos/MainActivity.kt
+++ app/src/main/java/com/juni/roverphotos/MainActivity.kt
@@ -2,39 +2,46 @@ package com.juni.roverphotos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.juni.roverphotos.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ListFragment.ItemSelectListener {
+
+    lateinit var photosFragment: PhotosFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.roverRecycle.layoutManager=LinearLayoutManager(this)
-        val divider= DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        binding.roverRecycle.addItemDecoration(divider)
 
-        val adapter=RoverAdapter()
-        binding.roverRecycle.adapter=adapter
+        photosFragment=supportFragmentManager.findFragmentById(R.id.rover_photos) as PhotosFragment
+       // binding.roverRecycle.layoutManager=LinearLayoutManager(this)
+
 
 
         val viewModel=ViewModelProvider(this).get(MainViewModel::class.java)
 
         viewModel.roverList.observe(this, Observer {
 
-            //Hi
 
-            it->        adapter.submitList(it)
+
 
         })
 
 
 
 
+
+    }
+
+    override fun onItemSelected(photos: Photos) {
+
+        photosFragment.setPhotos(photos)
 
     }
 }
